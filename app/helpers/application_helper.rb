953a0b970e4e83b034fd4dde3b38c5419ef1ab73
Taylor <taybrown9000@gmail.com>
2015-01-28 17:43:42 -0500

    Added Home page, no links work right now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    # Returns the full title on a per-page basis.
+    def full_title(page_title = '')
+        base_title = "Tremelo"
+
+        if page_title.empty?
+            base_title
+        else
+            "#{page_title} | #{base_title}" .html_safe
+        end
+    end
+
 end
