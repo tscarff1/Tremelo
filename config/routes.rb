2efb635f7e926739f703c9bc2b2d3366142b3cc8
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-01-28 15:50:04 -0500

    Fixed the help route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'static_pages#about'
 
-  get 'about' => 'static_pages#help'
+  get 'help' => 'static_pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
