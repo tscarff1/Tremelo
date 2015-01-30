6a5eae8e6eef6ac2f7e8e1d229fd112c1387c75f
Sean Winner <swinner2@gulls.salisbury.edu>
2015-01-29 23:02:54 -0500

    added notifier.rb to mailer and modified application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,46 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  add_flash_types :success
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActiveSupport::MessageVerifier::InvalidSignature, with: :render_error
+
+  private
+  def go_back_link_to(path)
+    @go_back_link_to ||= path
+    @go_back_link_to
+  end
+
+  def render_404
+    render file: 'public/404.html', status: :not_found, layout: false
+  end
+
+  def render_error
+    render file: 'public/500.html', status: :internal_server_error, layout: false
+  end
+  def logged_in?
+    current_user
+  end
+  helper_method :logged_in?
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    elsif cookies.permanent.signed[:remember_me_token]
+      verification = Rails.application.message_verifier(:remember_me).verify(cookies.permanent.signed[:remember_me_token])
+      if verification
+        Rails.logger.info "Logging in by cookie."
+        @current_user ||= User.find(verification)
+      end
+    end
+  end
+  helper_method :current_user
+
+  def require_user
+    if current_user
+      true
+    else
+      redirect_to new_user_session_path, notice: "You must be logged in to access that page."
+    end
+  end
 end
