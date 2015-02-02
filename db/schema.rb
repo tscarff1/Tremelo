04776c4ce4db9aceaece229678301e8073d0f26f
Taylor <taybrown9000@gmail.com>
2015-02-02 09:17:31 -0500

    Minor tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129045448) do
+ActiveRecord::Schema.define(version: 20150130051007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20150129045448) do
     t.string   "name"
     t.string   "location"
     t.string   "about_me"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "videos", force: true do |t|
+    t.string   "video_file_name"
+    t.string   "video_content_type"
+    t.integer  "video_file_size"
+    t.integer  "band_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
