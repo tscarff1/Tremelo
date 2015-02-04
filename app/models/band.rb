8320d083828917d023a86e7a223ae7488696d781
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-02-03 22:43:46 -0500

    Beginning band validations

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,11 @@
 class Band < ActiveRecord::Base
+	validates :name, 
+		presence: true,
+		uniqueness: true
+
+	before_save :downcase_name
+
+	def downcase_name
+		self.name = name.downcase
+	end
 end
