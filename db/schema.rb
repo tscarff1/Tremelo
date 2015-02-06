38d82be151fb483018af9c45b525fd1a5d81b846
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-02-06 01:03:22 -0500

    Added about_me to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206051837) do
+ActiveRecord::Schema.define(version: 20150206054124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150206051837) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "address"
+    t.string   "about_me"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
