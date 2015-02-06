6ad8930691ff224769ec886d834b02ccb3727769
Taylor <taybrown9000@gmail.com>
2015-02-06 09:27:09 -0500

    Fixed homepage button problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:display_name, :first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:display_name, :first_name, :last_name, :email, 
+                            :email_confirmation, :password, :password_confirmation)
     end
 end
