0f2501de9cd7bc9f188f244f7c5f1f9b716114b5
Taylor <taybrown9000@gmail.com>
2015-02-16 09:55:57 -0500

    Missed javascript file

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,10 @@
+$(function() {
+    $("form text").keypress(function (e) {
+        if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+            $('button[type=submit] .default').click();
+            return false;
+        } else {
+            return true;
+        }
+    });
+});
