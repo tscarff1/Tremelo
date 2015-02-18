240100b235e800291c63c452a3ac212ba47075b8
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-02-18 02:48:54 -0500

    Removed the video columns from the band model. Videos handled exclusively by the videoband model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218070523) do
+ActiveRecord::Schema.define(version: 20150218074816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,6 @@ ActiveRecord::Schema.define(version: 20150218070523) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "full_address"
-    t.string   "video_link"
-    t.string   "video_link_html"
   end
 
   create_table "tags", force: true do |t|
