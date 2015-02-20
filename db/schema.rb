1ed1d3ff5f3a03491f90a555aa4302fa29b7e10a
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-02-20 09:43:21 -0500

    Prepopulation of tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219062057) do
+ActiveRecord::Schema.define(version: 20150220143950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,13 +75,15 @@ ActiveRecord::Schema.define(version: 20150219062057) do
     t.string   "about_me"
     t.string   "home_address"
     t.string   "state"
-    t.string   "password_reset_token"
     t.string   "profile_picture_file_name"
     t.string   "profile_picture_content_type"
     t.integer  "profile_picture_file_size"
     t.datetime "profile_picture_updated_at"
+    t.string   "password_reset_token"
     t.date     "date_of_birth"
     t.integer  "zipcode"
+    t.string   "video_link"
+    t.string   "video_link_html"
     t.string   "city"
   end
 
