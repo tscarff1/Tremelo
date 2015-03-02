f1f11a594197b244d844a21b0140d492b2b67d5e
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-03-02 09:07:42 -0500

    ...

---
+++ db/migrate/20150302051924_load_tags.rb
@@ -0,0 +1,9 @@
+class LoadTags < ActiveRecord::Migration
+  def change
+  		tags = ['saxophone', 'guitar', 'vocals', 'drums', 'flute', 'fiddle', 'violin', 'banjo',
+  	'piano', 'keyboard', 'harmonica', 'bass']
+  	for tag_content in tags
+  		Tag.create(content: tag_content)
+  	end
+  end
+end
