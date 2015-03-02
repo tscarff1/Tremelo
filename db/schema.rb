8bb71edf136aef0a268a5a9a7e17dac60e5cce06
Taylor <taybrown9000@gmail.com>
2015-03-02 09:52:42 -0500

    Adding genres

--- db/schema.rb
+++ db/schema.rb
@@ -81,15 +81,13 @@ ActiveRecord::Schema.define(version: 20150302144521) do
     t.string   "about_me"
     t.string   "home_address"
     t.string   "state"
+    t.string   "password_reset_token"
     t.string   "profile_picture_file_name"
     t.string   "profile_picture_content_type"
     t.integer  "profile_picture_file_size"
     t.datetime "profile_picture_updated_at"
-    t.string   "password_reset_token"
     t.date     "date_of_birth"
     t.integer  "zipcode"
-    t.string   "video_link"
-    t.string   "video_link_html"
     t.string   "city"
   end
 
