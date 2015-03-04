f7d8fce5d79ce354ed766931a49f29842cc11ae2
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-03-04 09:21:49 -0500

    ?!

--- app/models/band.rb
+++ app/models/band.rb
@@ -9,6 +9,28 @@ class Band < ActiveRecord::Base
 	geocoded_by :full_address
 	after_validation :geocode, :if => :full_address_changed?
 
+	SOUNDCLOUD_CLIENT_ID = "957c34cadbaefe85378c2014b12227a5"
+	SOUNDCLOUD_CLIENT_SECRET = "71cecfc3f8620b57e03aec7b2b3e2cc3"
+
+	def self.SOUNDCLOUD_CLIENT_ID
+		SOUNDCLOUD_CLIENT_ID
+	end
+
+	def self.soundcloud_stuff
+		# register a client with YOUR_CLIENT_ID as client_id_
+		client = SoundCloud.new(:client_id => SOUNDCLOUD_CLIENT_ID)
+		# get 10 hottest tracks
+		tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
+		# print each link
+		
+	end
+
+	def self.printsoundcloud
+		tracks.each do |track|
+	  		puts track.permalink_url
+		end
+	end
+
 	#Call as Band.delete_empty_bands to delete any bands without any members
 	def self.delete_empty_bands
 		bands = Band.all
