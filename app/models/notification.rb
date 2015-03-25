2fd2961983f9c966a9cd7fb1d998f1f94248b0b6
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-03-25 09:05:54 -0400

    Fixed forms

---
+++ app/models/notification.rb
@@ -0,0 +1,5 @@
+class Notification < ActiveRecord::Base
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :content, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :notifications
 
   validates :email, presence: true,
                     uniqueness: true,
