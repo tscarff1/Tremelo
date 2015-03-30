fe88cc7d5356a63594d3e47a7d1361935b68a4cc
tscarff1 <tscarff1@gulls.salisbury.edu>
2015-03-30 09:24:00 -0400

    Notifications now have a special characters column

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,12 +1,33 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
-  validates :content, presence: true
+  validates :content, presence: true, uniqueness: true
 
-  validate :not_duplicate
+  # Special Characters List
+  # none - Generic Notification (ie, band uploaded a video)
+  # %B Band Invitation
+  # %b user requesting membership to a band
+  # %m Message
+  # %M Formatted message (Just an idea)
+  #  
+
+  def is_band_invite?
+  	if !special_chars.nil? && special_chars.include?("%B")
+  		return true
+  	end
+  	return false
+  end
+
+  def self.get_band_invites_for(user_id)
+  	notes = Notification.where(user_id: user_id)
+  	final_set = []
+  	for note in notes
+  		if note.is_band_invite?
+  			final_set.push(note)
+  		end
+  	end
+  	return final_set
+  end
 
   private
-
-  	def not_duplicate
-  	end
 end
