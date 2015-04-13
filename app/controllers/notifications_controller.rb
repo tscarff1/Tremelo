d28f411c1fbaa3c0648922b1d9b02d4371d09356
Taylor <taybrown9000@gmail.com>
2015-04-13 08:58:42 -0400

    Before merge

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,17 @@
 class NotificationsController < ApplicationController
+
+  # def create
+  #   @notifications = Notification.create!
+  # end
+
   def destroy
   	if (!params[:id].nil?) 
   		note = Notification.find(params[:id])
   		note.destroy
   		respond_to do |format|
-	        format.html { redirect_to notifications_users_path, success: 'Notification deleted' }
-	    end
-	end
-  end
+       format.html { redirect_to notifications_users_path, success: 'Notification deleted' }
+     end
+   end
+ end
+
 end
